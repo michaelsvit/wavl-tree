b0cc38d8c325bcbddba8b9f2a7d3a25965d56540
Michael Svit <michaelsvit@gmail.com>
2016-12-03 22:51:01 +0200

    Create double rotation function and add documentation

--- src/HelperFunctions.java
+++ src/HelperFunctions.java
@@ -1,32 +1,76 @@
 /**
  * Created by Michael on 12/3/2016.
+ * WAVLTree implementation helper functions.
  */
 public class HelperFunctions {
 
     /**
-     * Rotates subtree around the edge connecting parent and child.
+     * Rotates subtree around the edge connecting node1 and node2.
      *
-     * @param parent parent node to rotate around
-     * @param child child node that would become parent
+     * @param node1 parent node to rotate around
+     * @param node2 child node that would become parent
      */
-    private void rotate(WAVLNode parent, WAVLNode child) {
-        if (child == parent.leftChild) {
-            rotateRight(parent, child);
-        } else if (child == parent.rightChild) {
-            rotateLeft(parent, child);
+    private void rotate(WAVLNode node1, WAVLNode node2) {
+        WAVLNode node1Parent = node1.parent; // temporarily save so it's not lost on rotation
+
+        if (node2 == node1.leftChild) {
+            rotateRight(node1, node2);
+        } else if (node2 == node1.rightChild) {
+            rotateLeft(node1, node2);
+        }
+
+        // Fix parent pointers
+        node1.parent = node2;
+        node2.parent = node1Parent;
+
+        // If not at the tree's root, fix node1's child pointer
+        if (node1Parent != null) {
+            if (node1Parent.leftChild == node1) {
+                node1Parent.leftChild = node2;
+            } else if(node1Parent.rightChild == node1) {
+                node1Parent.rightChild = node2;
+            }
         }
     }
 
-    private void rotateLeft(WAVLNode parent, WAVLNode child) {
-        WAVLNode childLeftChild = child.leftChild;
-        child.leftChild = parent;
-        parent.rightChild = childLeftChild;
+    /**
+     * Performs a left-rotation on the subtree around the edge connecting node1 and node2.
+     *
+     * @param node1 parent node to rotate around
+     * @param node2 child node that would become parent
+     */
+    private void rotateLeft(WAVLNode node1, WAVLNode node2) {
+        WAVLNode node2LeftChild = node2.leftChild; // temporarily save so it's not lost on rotation
+
+        // Reassign pointers
+        node2.leftChild = node1;
+        node1.rightChild = node2LeftChild;
     }
 
-    private void rotateRight(WAVLNode parent, WAVLNode child) {
-        WAVLNode childRightChild = child.rightChild;
-        child.rightChild = parent;
-        parent.leftChild = childRightChild;
+    /**
+     * Performs a left-rotation on the subtree around the edge connecting node1 and node2.
+     *
+     * @param node1 parent node to rotate around
+     * @param node2 child node that would become parent
+     */
+    private void rotateRight(WAVLNode node1, WAVLNode node2) {
+        WAVLNode node2RightChild = node2.rightChild; // temporarily save so it's not lost on rotation
+
+        // Reassign pointers
+        node2.rightChild = node1;
+        node1.leftChild = node2RightChild;
+    }
+
+    /**
+     * Performs a double rotation, first on node2 and node3, and then on node1 and node2.
+     *
+     * @param node1 highest node
+     * @param node2 middle node
+     * @param node3 lowest node
+     */
+    private void doubleRotate(WAVLNode node1, WAVLNode node2, WAVLNode node3) {
+        rotate(node2, node3);
+        rotate(node1, node2);
     }
 
     public class WAVLNode {
