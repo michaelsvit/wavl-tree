075aeb9359eb959cf0de6868a5c1eb2b6c35b154
Michael Svit <michaelsvit@gmail.com>
2016-12-04 12:53:00 +0200

    Update skeleton source file with helper methods and reformat code

--- src/WAVLTree.java
+++ src/WAVLTree.java
@@ -1,272 +1,289 @@
 /**
- *
  * WAVLTree
- *
+ * <p>
  * An implementation of a WAVL Tree with
  * distinct integer keys and info
- *
  */
 
 public class WAVLTree {
-	
-	public WAVLNode sentinel;
-	public WAVLNode root;
-	//public WAVLNode null;
-	
-	public WAVLTree()
-	{
-		this.root = null;	
-		
-	}
-	
 
-  /**
-   * public boolean empty()
-   *
-   * returns true if and only if the tree is empty
-   *
-   */
-  public boolean empty() {
-	  if (this.root == null)
-	  {
-		  return true;
-	  }
-	  
-    return false; 
-  }
+    private WAVLNode root;
+    private final WAVLNode externalLeaf; // assigned to be the bottom node of every route in the tree
 
- /**
-   * public String search(int k)
-   *
-   * returns the info of an item with key k if it exists in the tree
-   * otherwise, returns null
-   */
-  public String search(int k)
-  {
-	  if (this.empty())
-	  {
-		  return null;
-	  }
-  		  
-	  WAVLNode result = searchRecursive(root, k);
-	  if(result != null){
-		  return result.Info;
-	  }
-	  else{
-		  return null;
-	  }
-	  
-  }
+    public WAVLTree() {
+        this.root = null;
+        this.externalLeaf = new WAVLNode();
+    }
 
-  /**
-   * public int insert(int k, String i)
-   *
-   * inserts an item with key k and info i to the WAVL tree.
-   * the tree must remain valid (keep its invariants).
-   * returns the number of rebalancing operations, or 0 if no rebalancing operations were necessary.
-   * returns -1 if an item with key k already exists in the tree.
-   */
-   public int insert(int k, String i) {
-	  return 42;	// to be replaced by student code
-   }
+    /**
+     * public boolean empty()
+     * <p>
+     * returns true if and only if the tree is empty
+     */
+    public boolean empty() {
+        return this.root == null;
+    }
 
-  /**
-   * public int delete(int k)
-   *
-   * deletes an item with key k from the binary tree, if it is there;
-   * the tree must remain valid (keep its invariants).
-   * returns the number of rebalancing operations, or 0 if no rebalancing operations were needed.
-   * returns -1 if an item with key k was not found in the tree.
-   */
-   public int delete(int k)
-   {
-	   return 42;	// to be replaced by student code
-   }
+    /**
+     * public String search(int k)
+     * <p>
+     * returns the info of an item with key k if it exists in the tree
+     * otherwise, returns null
+     */
+    public String search(int k) {
+        if (empty()) {
+            return null;
+        }
 
-   /**
-    * public String min()
-    *
-    * Returns the i׳�fo of the item with the smallest key in the tree,
-    * or null if the tree is empty
-    */
-   public String min()
-   {
-	   if (this.empty()){
-		   return null; 
-	   }
-	   WAVLNode iter = root;
-	   while ( iter.Lchild!=null ){
-		   iter = iter.Lchild;
-	   }
-	   return iter.Info;
-   }
+        WAVLNode result = searchRecursive(root, k);
+        if (result != null) {
+            return result.info;
+        } else {
+            return null;
+        }
+    }
 
-   /**
-    * public String max()
-    *
-    * Returns the info of the item with the largest key in the tree,
-    * or null if the tree is empty
-    */
-   public String max()
-   {
-	   if (this.empty()){
-		   return null; 
-	   }
-	   WAVLNode iter = root;
-	   while ( iter.Rchild!=null ){
-		   iter = iter.Rchild;
-	   }
-	   return iter.Info;
-   }
+    /**
+     * public int insert(int k, String i)
+     * <p>
+     * inserts an item with key k and info i to the WAVL tree.
+     * the tree must remain valid (keep its invariants).
+     * returns the number of rebalancing operations, or 0 if no rebalancing operations were necessary.
+     * returns -1 if an item with key k already exists in the tree.
+     */
+    public int insert(int k, String i) {
+        return 42;    // to be replaced by student code
+    }
 
-  /**
-   * public int[] keysToArray()
-   *
-   * Returns a sorted array which contains all keys in the tree,
-   * or an empty array if the tree is empty.
-   */
-  public int[] keysToArray()
-  {
-	  if (this.empty()){
-		   return new int[0]; 
-	   }
-	  
-	   WAVLNode iter = root;
-	   
-	   
-	   
+    /**
+     * public int delete(int k)
+     * <p>
+     * deletes an item with key k from the binary tree, if it is there;
+     * the tree must remain valid (keep its invariants).
+     * returns the number of rebalancing operations, or 0 if no rebalancing operations were needed.
+     * returns -1 if an item with key k was not found in the tree.
+     */
+    public int delete(int k) {
+        return 42;    // to be replaced by student code
+    }
+
+    /**
+     * public String min()
+     * <p>
+     * Returns the info of the item with the smallest key in the tree,
+     * or null if the tree is empty
+     */
+    public String min() {
+        if (empty()) {
+            return null;
+        }
+
+        WAVLNode iterator = root;
+        while (iterator.leftChild != null) {
+            iterator = iterator.leftChild;
+        }
+        return iterator.info;
+    }
+
+    /**
+     * public String max()
+     * <p>
+     * Returns the info of the item with the largest key in the tree,
+     * or null if the tree is empty
+     */
+    public String max() {
+        if (empty()) {
+            return null;
+        }
+
+        WAVLNode iterator = root;
+        while (iterator.rightChild != null) {
+            iterator = iterator.rightChild;
+        }
+        return iterator.info;
+    }
+
+    /**
+     * public int[] keysToArray()
+     * <p>
+     * Returns a sorted array which contains all keys in the tree,
+     * or an empty array if the tree is empty.
+     */
+    public int[] keysToArray() {
+        if (this.empty()) {
+            return new int[0];
+        }
+
+        WAVLNode iter = root;
+
+
         int[] arr = new int[42]; // to be replaced by student code
         return arr;              // to be replaced by student code
-  }
+    }
 
-  /**
-   * public String[] infoToArray()
-   *
-   * Returns an array which contains all info in the tree,
-   * sorted by their respective keys,
-   * or an empty array if the tree is empty.
-   */
-  public String[] infoToArray()
-  {
+    /**
+     * public String[] infoToArray()
+     * <p>
+     * Returns an array which contains all info in the tree,
+     * sorted by their respective keys,
+     * or an empty array if the tree is empty.
+     */
+    public String[] infoToArray() {
         String[] arr = new String[42]; // to be replaced by student code
         return arr;                    // to be replaced by student code
-  }
+    }
 
-   /**
-    * public int size()
-    *
-    * Returns the number of nodes in the tree.
-    *
-    * precondition: none
-    * postcondition: none
-    */
-   public int size()
-   {
-	   return 42; // to be replaced by student code
-   }
+    /**
+     * public int size()
+     * <p>
+     * Returns the number of nodes in the tree.
+     * <p>
+     * precondition: none
+     * postcondition: none
+     */
+    public int size() {
+        return 42; // to be replaced by student code
+    }
 
-  /**
-   * public class WAVLNode
-   *
-   * If you wish to implement classes other than WAVLTree
-   * (for example WAVLNode), do it in this file, not in 
-   * another file.
-   * This is an example which can be deleted if no such classes are necessary.
- * @return 
-   */
-   
 
-   private WAVLNode searchRecursive( WAVLNode root, int k){
-	   if (root==null){
-		   return null; 
-	   }
-	   else if (root.Key==k ){
-		   return root;
-	   }
-	   else if (root.Key<k){
-		   return searchRecursive(root.Lchild, k);
-	   }
-	   else if (root.Key>k){
-		   return searchRecursive(root.Rchild, k);
-	   }   
-	   return null;
-	}
-   
-   
-   
-   
-   
-  public class WAVLNode{
-	  
-	  private WAVLNode Parent ;
-	  private WAVLNode Rchild ;
-	  private WAVLNode Lchild ;
-	  private int Key ;
-	  private String Info;
-	  
-	  public WAVLNode (WAVLNode parent, WAVLNode rchild,
-			  WAVLNode lchild, int key, String info )
-	  {
-		  this.Parent = parent;
-		  this.Rchild = rchild;
-		  this.Lchild = lchild;
-		  this.Key = key;
-		  this.Info = info;
-	  }
-	  
-	  public WAVLNode ()
-	  {
-		  this.Parent = this;
-		  this.Rchild = this;
-		  this.Lchild = this;
-		  this.Key = Integer.MAX_VALUE;
-		  this.Info = "";
-	  }
-	
-	  public WAVLNode getParent() {
-		  return Parent;
-		}
-	
-		public void setParent(WAVLNode parent) {
-			Parent = parent;
-		}
-	
-		public WAVLNode getRchild() {
-			return Rchild;
-		}
-	
-		public void setRchild(WAVLNode rchild) {
-			Rchild = rchild;
-		}
-	
-		public WAVLNode getLchild() {
-			return Lchild;
-		}
-	
-		public void setLchild(WAVLNode lchild) {
-			Lchild = lchild;
-		}
-	
-		public int getKey() {
-			return Key;
-		}
-	
-		public void setKey(int key) {
-			Key = key;
-		}
-	
-		public String getInfo() {
-			return Info;
-		}
-	
-		public void setInfo(String info) {
-			Info = info;
-		}
-	  
+    // ************************************* Helper functions *************************************************
 
-		
-  }
- 
+
+    /**
+     * Searches recursively for a node with the given key.
+     *
+     * @param root root of the current subtree
+     * @param key  key of the node to look for
+     * @return     node with the specified key, or null if key was not found
+     */
+    private WAVLNode searchRecursive(WAVLNode root, int key) {
+        if (root == null) {
+            return null;
+        } else if (root.key == key) {
+            return root;
+        } else if (root.key < key) {
+            return searchRecursive(root.leftChild, key);
+        } else if (root.key > key) {
+            return searchRecursive(root.rightChild, key);
+        }
+        return null;
+    }
+
+    /**
+     * Rotates subtree around the edge connecting node1 and node2.
+     * Assumes node1 is node2's parent.
+     *
+     * @param node1 parent node to rotate around
+     * @param node2 child node that would become parent
+     */
+    private void rotate(WAVLNode node1, WAVLNode node2) {
+        WAVLNode node1Parent = node1.parent; // temporarily save so it's not lost on rotation
+
+        if (node2 == node1.leftChild) {
+            rotateRight(node1, node2);
+        } else {
+            rotateLeft(node1, node2);
+        }
+
+        // Fix parent pointers
+        node1.parent = node2;
+        node2.parent = node1Parent;
+
+        // If not at the tree's root, fix node1's child pointer
+        if (node1Parent != null) {
+            if (node1Parent.leftChild == node1) {
+                node1Parent.leftChild = node2;
+            } else {
+                node1Parent.rightChild = node2;
+            }
+        }
+    }
+
+    /**
+     * Performs a left-rotation on the subtree around the edge connecting node1 and node2.
+     *
+     * @param node1 parent node to rotate around
+     * @param node2 child node that would become parent
+     */
+    private void rotateLeft(WAVLNode node1, WAVLNode node2) {
+        WAVLNode node2LeftChild = node2.leftChild; // temporarily save so it's not lost on rotation
+
+        // Reassign pointers
+        node2.leftChild = node1;
+        node1.rightChild = node2LeftChild;
+    }
+
+    /**
+     * Performs a left-rotation on the subtree around the edge connecting node1 and node2.
+     *
+     * @param node1 parent node to rotate around
+     * @param node2 child node that would become parent
+     */
+    private void rotateRight(WAVLNode node1, WAVLNode node2) {
+        WAVLNode node2RightChild = node2.rightChild; // temporarily save so it's not lost on rotation
+
+        // Reassign pointers
+        node2.rightChild = node1;
+        node1.leftChild = node2RightChild;
+    }
+
+    /**
+     * Performs a double rotation, first on node2 and node3, and then on node1 and node2.
+     *
+     * @param node1 highest node
+     * @param node2 middle node
+     * @param node3 lowest node
+     */
+    private void doubleRotate(WAVLNode node1, WAVLNode node2, WAVLNode node3) {
+        rotate(node2, node3);
+        rotate(node1, node2);
+    }
+
+    /**
+     * A single tree-node that holds given String info
+     */
+    public class WAVLNode {
+
+        private WAVLNode parent;
+        private WAVLNode leftChild;
+        private WAVLNode rightChild;
+        private int key;
+        private String info;
+        private int rankDiff; // difference in rank between current node and its parent
+
+        private WAVLNode(WAVLNode parent, WAVLNode rightChild, WAVLNode leftChild, int key, String info) {
+            this.parent = parent;
+            this.rightChild = rightChild;
+            this.leftChild = leftChild;
+            this.key = key;
+            this.info = info;
+            this.rankDiff = 1;
+        }
+
+        private WAVLNode() {
+            this.parent = null;
+            this.rightChild = null;
+            this.leftChild = null;
+            this.key = -1;
+            this.info = null;
+            this.rankDiff = 1;
+        }
+
+        /**
+         * Increases current node's rank by 1 by decreasing rank difference to parent's rank.
+         */
+        public void promote() {
+            rankDiff--;
+        }
+
+        /**
+         * Decreases current node's rank by 1 by increasing rank difference to parent's rank.
+         */
+        public void demote() {
+            rankDiff++;
+        }
+    }
+
 
 }
   
